d8c452cbabca0d715ec2bc5041a31bd049ca2503
alejandroacostaalamo2 <31591280+alejandroacostaalamo2@users.noreply.github.com>
2017-09-21 19:53:23 -0300

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,5 @@
 import pymysql
 def dbconnect():
-    connection = pymysql.connect(host='localhost', user='root', passwd='rc.353', db='COURSERA', charset='utf8')
+    connection = pymysql.connect(host='localhost', user='root', passwd='coursera', db='COURSERA', charset='utf8')
     return connection
 
